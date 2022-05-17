5d7653d213c479f546f1aa5efc1b8f0b1ec4227f
Igor Shpykuliak <defleppard333@gmail.com>
2022-05-17 21:36:34 +0300

    fix: task 1 add readStream (Tue, May 17, 2022  9:36:34 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,19 @@
-const fs = require("fs");
+ const fs = require("fs");
  
+//! readFile
 
-fs.readFile("01-read-file/text.txt", "utf8", 
-            function(error,data){
-                if(error) throw error; 
-                console.log(data);  
-});
+// fs.readFile("01-read-file/text.txt", "utf8", 
+//             function(error,data){
+//                 if(error) throw error; 
+//                 console.log(data);  
+// });
+
+
+//!  readStream
+
+const stream = fs.createReadStream(
+    '01-read-file/text.txt',
+    'utf8'
+  );
+  stream.on('data', (data) => console.log(data));
+  stream.on('error', (err) => console.log(`Err: ${err}`));
