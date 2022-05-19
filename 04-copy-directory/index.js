5ff29596b00184789cb9f55db9520fe327b3b2ba
Igor Shpykuliak <defleppard333@gmail.com>
2022-05-20 01:09:07 +0300

    feat: 4th task is done (Fri, May 20, 2022  1:09:06 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+const dir = path.join(__dirname, 'files');
+const dirCopy = path.join(__dirname, 'files-copy');
+
+(async () => {
+  await fs.rm(dirCopy, { recursive: true, force: true });
+  await fs.mkdir(dirCopy,);
+  const files = await fs.readdir(dir, { withFileTypes: true });
+  files.forEach(async (file) => {
+    if (file.isFile()) {
+      await fs.copyFile(path.join(dir, file.name), path.join(dirCopy, file.name));
+    }
+  });
+})();
